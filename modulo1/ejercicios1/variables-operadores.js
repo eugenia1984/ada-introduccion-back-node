eca73ebd1e77adb4c8d163908532597731aac283
Eugenia <costamariaeugenia1@gmail.com>
2022-08-14 09:07:43 -0300

    cambios los while por if, para que solo avise una vez que esta mal y se cierre

--- modulo1/ejercicios1/variables-operadores.js
+++ modulo1/ejercicios1/variables-operadores.js
@@ -121,15 +121,12 @@ function getDogYears() {
   let dogAge;
   let text;
 
-  /* RegEx \D 
-  (non-digit) matches any single character that is not a digit (same as [^0-9] )
-  */
-  while (humanAge == null || 
+  /* RegEx : \D -> (non-digit) matches any single character that is not a digit (same as [^0-9] ) */
+  if (humanAge == null || 
     /\D/.test(humanAge) || 
     humanAge == "" || 
     humanAge ==  0) {
     alert("Debes ingresar un numero.");
-    humanAge = prompt('Ingresa tus años: ');
   };
   
   dogAge = humanAge * 7;
@@ -141,12 +138,11 @@ function getMinutesToSeconds() {
   let minutes = prompt('Ingresa los minutos para pasarlos a segundos: ');
   let seconds;
 
-  while (minutes == null || 
+  if (minutes == null || 
     /\D/.test(minutes) || 
     minutes == "" || 
     minutes ==  0) {
     alert("Debes ingresar un numero.");
-    minutes = prompt('Ingresa los minutos para pasarlos a segundos: ');
   };
 
   seconds = minutes * 60;
@@ -158,12 +154,11 @@ function getDaysToSeconds() {
   let days = prompt('Ingresa los días para pasarlos a segundos: ');
   let seconds;
 
-  while (days == null || 
+  if (days == null || 
     /\D/.test(days) || 
     days == "" || 
     days ==  0 ) {
     alert("Debes ingresar un numero.");
-    days = prompt('Ingresa los días para pasarlos a segundos: ');
   };
 
   seconds = days * 24 * 60 * 60;
@@ -174,17 +169,15 @@ function getDaysToSeconds() {
 function getKilometersToMilles() {
   let kilometers = prompt('Ingresa los kilometros para pasarlos a millas: ');
 
-  while (kilometers == null || 
+  if (kilometers == null || 
     /\D/.test(kilometers) || 
     kilometers == "" || 
     kilometers ==  0 ) {
     alert("Debes ingresar un numero.");
-    kilometers = prompt('Ingresa los kilometros para pasarlos a millas: ');
   };
 
   miles =  getTwoDecimalsRound(kilometers * 0.62137);
   text = `El/Los ${kilometers} kilometro(s) ingresados equivalen a ${miles} millas.`;
-
   d.getElementById("kilometros-a-millas").innerHTML = text;
 }
 
@@ -192,18 +185,15 @@ function getTriangleArea() {
   let base = prompt('Ingresa la base del triangulo : ');
   let height = prompt('Ingresa la altura del triangulo : ');
 
-  while (base == null || height == null ||
+  if (base == null || height == null ||
     /\D/.test(base) || /\D/.test(height) ||
     base == "" || height == "" ||
     base ==  0 || height ==  0 ) {
     alert("Debes ingresar un numero para la base y la altura.");
-    base = prompt('Ingresa la base del triangulo : ');
-    height = prompt('Ingresa la altura del triangulo : ');
   };
 
   area = getTwoDecimalsRound((base * height) / 2);
   text = `El área de un triangulo con ${base} de base y ${height} de altura es: ${area}.`;
-
   d.getElementById("area-de-triangulo").innerHTML = text;
 }
 
@@ -211,18 +201,15 @@ function getPerimeterRectangle() {
   let base = prompt('Ingresa la base del rectangulo : ');
   let height = prompt('Ingresa la altura del rectangulo : ');
 
-  while (base == null || height == null ||
+  if (base == null || height == null ||
     /\D/.test(base) || /\D/.test(height) ||
     base == "" || height == "" ||
     base ==  0 ||height ==  0) {
     alert("Debes ingresar un numero para la base y la altura.");
-    base = prompt('Ingresa la base del rectangulo : ');
-    height = prompt('Ingresa la altura del rectangulo : ');
   };
 
   perimeter = base*2 +  height*2;
   text = `El perímetro de un rectángulo con ${base} de base y ${height} de altura es: ${perimeter}.`;
-
   d.getElementById("perimetro-de-rectangulo").innerHTML = text;
 }
 
@@ -230,31 +217,27 @@ function getPercentage() {
   let number = prompt('Ingresa un número : ');
   let percentageToCalculate = prompt('Ingresa el porcentaje que deseas obtener del número antes ingresado: : ');
 
-  while (number == null || percentageToCalculate == null ||
+  if (number == null || percentageToCalculate == null ||
     /\D/.test(number) || /\D/.test(percentageToCalculate) ||
     number == "" || percentageToCalculate == "" ||
     number ==  0 ||percentageToCalculate ==  0) {
     alert("Debes ingresar un numero para la base y la altura.");
-    number = prompt('Ingresa un número : ');
-    percentageToCalculate = prompt('Ingresa el porcentaje que deseas obtener del número antes ingresado: : ');
   };
 
   percentageRound = getTwoDecimalsRound( (percentageToCalculate * number )/100
   ); 
   text = `El ${percentageToCalculate} % de ${number} es: ${percentageRound}.`;
-  
   d.getElementById("obtener-porcentaje").innerHTML = text;
 }
 
 
 function getTravelTime() {
   let distance = prompt("Ingrese la distancia a recorrer -en kilometros- :")
-  while (distance == null ||
+  if (distance == null ||
     /\D/.test(distance) || 
     distance == "" || 
     distance ==  0) {
       alert("Debes ingresar un numero.")
-      distance = prompt("Ingrese la distancia a recorrer -en kilometros- : ");
   };
   // on foot 5km/h
   distanceOnFoot = getTwoDecimalsRound((distance*1) /5);
