bb1d5f6776877ad4c2e157f4371555259df184d1
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-08-15 14:26:53 -0300

    agrego los demas ejercicios de la segunda practica

--- modulo1/ejercicios2/index.js
+++ modulo1/ejercicios2/index.js
@@ -1,17 +1,43 @@
 const d = document;
+const onlyVocals = /^[A,E,I,O,U,a,e,i,o,u,Á,É,Í,Ó,Ú,á,é,í,ó,ú]$/
+const onlyConsonants = /^[B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z,b,c,d,f,g,h,j,k,l,m,n,s,t,v,w,x,y,z]$/
+/*RegEx
+^ indica que el patrón debe iniciar con los caracteres dentro de los corchetes
++ indica que los caracteres dentro de los corchetes se pueden repetir
+*/
 
 function puedeVerPelicula(edad, tieneAutorizacion) {
   return (edad >= 15 || tieneAutorizacion == true) ? "La persona está autorizada" : "La persona NO está autorizada";
 }
 
 function estaEnRango(valor, minimo, maximo) {
-  if (valor == null || valor == "" || isNaN(valor) ) {
+  if (valor == null || valor == "" || isNaN(valor)) {
     return "Debes ingresar un numero";
   } else {
     return (valor > minimo && valor < maximo)? `${valor} está entre ${maximo} y ${minimo}.`: `Esta fuera de rango`;
   }
 }
 
+function puedeAvanzar(colorSemaforo) {
+  if(colorSemaforo == "verde") {
+    console.log("Puede avanzar");
+    return "Puede avanzar";
+  } else if(colorSemaforo == "amarillo" || colorSemaforo == "rojo") {
+    console.log("No avance");
+    return "No avance";
+  } else {
+    console.log("Error: color de semáforo inválido<");
+    return "Error: color de semáforo inválido<";
+  }
+}
+
+function esVocal(letra) {
+  return (onlyVocals.test(letra))? `${letra} es una vocal`: `No es una vocal`;
+}
+
+function esConsonante(letra) {
+  return (onlyConsonants.test(letra))? `${letra} es una consonante`: `No es una consonante`;
+}
 function eventoPuedeVerPelicula() {
   let age = Number(prompt("Ingresa tu edad -en numeros-: "));
   let tieneAutorizacion = false;
@@ -33,3 +59,25 @@ function eventoEstaEnRango() {
   d.getElementById("esta-en-rango").innerHTML = text;
 }
 
+function eventoPuedeAvanzar() {
+  let color = (prompt("Ingresa el color del semaforo para indicarle si puede avanzar: ")).toLocaleLowerCase();
+  text = puedeAvanzar(color);
+  d.getElementById("puede-avanzar").innerHTML = text;
+}
+
+function eventoEsVocal() {
+  let vocal = (prompt("Ingresa una letra para decirte si es vocal: ")).toLocaleLowerCase();
+  let text= ``;
+  if(vocal == null || vocal == ""){
+    alert("debe ingresar una letra");
+  } else {
+    text = esVocal(vocal);
+  }
+  d.getElementById("es-vocal").innerHTML = text;
+}
+
+function eventoEsConsonante() {
+  let consonante = (prompt("Ingresa una letra para ver si es consonante: ")).toLocaleLowerCase();
+  text = esConsonante(consonante);
+  d.getElementById("es-consonante").innerHTML = text;
+}
