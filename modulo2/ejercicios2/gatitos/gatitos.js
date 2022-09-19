c882a78a7aaf50fd7fa4a9a7a83f799c123ae03a
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-09-19 08:55:44 -0300

    modifico el JSON de gatitos lo paso a un archivo y lo ecporto asi el app.get queda mas corto

---
+++ modulo2/ejercicios2/gatitos/gatitos.js
@@ -0,0 +1,39 @@
+export default {
+  "big" : [
+    "http://i.imgur.com/CRSzQ4r.png", 
+    "http://i.imgur.com/l94Rfsj.png", 
+    "http://i.imgur.com/304PJ9p.png", 
+    "http://i.imgur.com/mOnnvms.png", 
+    "http://i.imgur.com/k5Eif3W.png", 
+    "http://i.imgur.com/xSVGJu4.png"
+  ],
+  "evil" : [
+    "http://i.imgur.com/4s6OJQj.png",
+     "http://i.imgur.com/su9edl7.png",
+     "http://i.imgur.com/bCgGgBM.png"
+  ],
+  "grumpy" : [
+    "http://i.imgur.com/EPGllv4.png",
+     "http://i.imgur.com/DKFbRQ0.png", 
+     "http://i.imgur.com/bIqsqSK.png"
+  ],
+  "inHats" : [
+    "http://i.imgur.com/6XcjacS.png", 
+    "http://i.imgur.com/GC3lgzg.png", 
+    "http://i.imgur.com/q7MZgiG.png", 
+    "http://i.imgur.com/VkfIH4a.png", 
+    "http://i.imgur.com/zy9cJgS.png", 
+    "http://i.imgur.com/OA4rZbW.png", 
+    "http://i.imgur.com/w0SzOoY.png", 
+    "http://i.imgur.com/zVZXKK4.png", 
+    "http://i.imgur.com/ZTn8bgI.png", 
+    "http://i.imgur.com/iVWFUlj.png"
+  ],
+  "kitten" : [
+    "http://i.imgur.com/KqFuFSQ.png", 
+    "http://i.imgur.com/H3YmnA3.png",
+     "http://i.imgur.com/qqWyfvA.png", 
+     "http://i.imgur.com/GS6HUnP.png", 
+     "http://i.imgur.com/bTvoEeb.png"
+  ]
+}
--- modulo2/ejercicios2/gatitos/index.js
+++ modulo2/ejercicios2/gatitos/index.js
@@ -4,6 +4,7 @@ import express from 'express'; // importamos la libreria express
 // para el punto 2
 import path from 'path';
 import {fileURLToPath } from 'url';
+import gatitos from './gatitos.js'
 
 const app = express();  // creamos una instancia de express
 const PORT = 3030; // aca me guardo el puerto en el que veo mi server
@@ -31,47 +32,7 @@ app.get('/mostrarGatito', (req,res) => {
 // Punto 3 
 app.get('/fotosGatitos', (req, res) => {
   // con esto devuevlo un JSON me falta ver como devolver el header
-  res.status(200).json(
-    {
-      "big" : [
-        "http://i.imgur.com/CRSzQ4r.png", 
-        "http://i.imgur.com/l94Rfsj.png", 
-        "http://i.imgur.com/304PJ9p.png", 
-        "http://i.imgur.com/mOnnvms.png", 
-        "http://i.imgur.com/k5Eif3W.png", 
-        "http://i.imgur.com/xSVGJu4.png"
-      ],
-      "evil" : [
-        "http://i.imgur.com/4s6OJQj.png",
-         "http://i.imgur.com/su9edl7.png",
-         "http://i.imgur.com/bCgGgBM.png"
-      ],
-      "grumpy" : [
-        "http://i.imgur.com/EPGllv4.png",
-         "http://i.imgur.com/DKFbRQ0.png", 
-         "http://i.imgur.com/bIqsqSK.png"
-      ],
-      "inHats" : [
-        "http://i.imgur.com/6XcjacS.png", 
-        "http://i.imgur.com/GC3lgzg.png", 
-        "http://i.imgur.com/q7MZgiG.png", 
-        "http://i.imgur.com/VkfIH4a.png", 
-        "http://i.imgur.com/zy9cJgS.png", 
-        "http://i.imgur.com/OA4rZbW.png", 
-        "http://i.imgur.com/w0SzOoY.png", 
-        "http://i.imgur.com/zVZXKK4.png", 
-        "http://i.imgur.com/ZTn8bgI.png", 
-        "http://i.imgur.com/iVWFUlj.png"
-      ],
-      "kitten" : [
-        "http://i.imgur.com/KqFuFSQ.png", 
-        "http://i.imgur.com/H3YmnA3.png",
-         "http://i.imgur.com/qqWyfvA.png", 
-         "http://i.imgur.com/GS6HUnP.png", 
-         "http://i.imgur.com/bTvoEeb.png"
-      ]
-    }
-  );
+  res.status(200).json(gatitos);
 });
 
 // Punto 4
